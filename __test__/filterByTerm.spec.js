08d81a8f8e7add1d31c561416d3502fc73e945d4
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-09-16 14:29:15 +0700

    Added: error handling for empty searchTerm

--- __test__/filterByTerm.spec.js
+++ __test__/filterByTerm.spec.js
@@ -1,4 +1,7 @@
 function filterByTerm(inputArr, searchTerm) {
+    if (searchTerm === '' || null || undefined) {
+       return [] 
+    }
     const regex = new RegExp(searchTerm, "i") // case insensitive matching
     return inputArr.filter(function(arrayElement) {
         return arrayElement.url.match(regex)
