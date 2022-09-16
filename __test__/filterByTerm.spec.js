aaefea6cb117667b82badd0947ee48fff6a3dca4
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-09-16 14:25:10 +0700

    Added a test case for the search term 'url'

--- __test__/filterByTerm.spec.js
+++ __test__/filterByTerm.spec.js
@@ -5,19 +5,30 @@ function filterByTerm(inputArr, searchTerm) {
     })
 }
 
+const input = [
+    { id: 1, url: 'https://www.url1.dev' },
+    { id: 2, url: 'https://www.url2.dev' },
+    { id: 3, url: 'https://www.link3.dev' }
+]
+
 describe('Filter function', () => {
     test('it should filter by a search term (link)', () => {
-        const input = [
-            { id: 1, url: 'https://www.url1.dev' },
-            { id: 2, url: 'https://www.url2.dev' },
-            { id: 3, url: 'https://www.link3.dev' }
-        ]
-
         const output = [
             { id: 3, url: 'https://www.link3.dev' }
         ]
 
         expect(filterByTerm(input, 'link')).toEqual(output)
         expect(filterByTerm(input, 'Link')).toEqual(output)
+    }),
+
+    test('it should filter by a search term (url)', () => {
+        const expected = [
+            { id: 1, url: 'https://www.url1.dev' },
+            { id: 2, url: 'https://www.url2.dev' }
+        ]
+
+        expect(filterByTerm(input, 'uRl')).toEqual(expected)
+        expect(filterByTerm(input, 'URL')).toEqual(expected)
+        expect(filterByTerm(input, 'url')).toEqual(expected)
     })
 })
