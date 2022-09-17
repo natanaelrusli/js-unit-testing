573e6ec6f70e94411d0cdc0f756e5cd2eb9c2cfe
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2022-09-17 21:05:45 +0700

    add: failing test for arrSum function

---
+++ __test__/arraySum.test.js
@@ -0,0 +1,10 @@
+import arraySum from '../src/arraySum'
+
+test('should return 5 when the input are [1, 2, 2]', () => {
+    const input = [1, 2, 3]
+    const expected = 5
+
+    const result = arraySum(input)
+
+    expect(result).toBe(expected)
+})
